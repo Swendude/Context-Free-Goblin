5265b75109240a91a16577247a918808c777132f
Swen Mulderij <swenmulderij@gmail.com>
2020-03-12 23:06:17 +0100

    functioning 'parser'

--- src/Generator.elm
+++ src/Generator.elm
@@ -1,64 +1,142 @@
-module Generator exposing (..)
-import Dict exposing (Dict)
-import Debug
-import Parser exposing (..)
-type Grammar = Grammar (Dict String (List String))
+module Generator exposing (Grammar(..), ProdPart(..), Production(..), Rule(..), RuleError(..), addProduction, addRule, empty, generator, parseRule, prodParser)
 
-empty = Grammar Dict.empty
+import Debug
+import Dict exposing (Dict)
+import Parser exposing (..)
+import Regex
+
+
+type Grammar
+    = Grammar (Dict String (List String))
+
+
+empty =
+    Grammar Dict.empty
+
 
 generator : Grammar -> String
-generator c = ""  
+generator c =
+    ""
 
 
-addProduction : String -> Maybe (List String)  -> Maybe (List String)
-addProduction new old = 
-    case old of 
+addProduction : String -> Maybe (List String) -> Maybe (List String)
+addProduction new old =
+    case old of
         Nothing ->
-            Just [new]
+            Just [ new ]
+
         Just rules ->
-            Just <| (new) :: rules
+            Just <| new :: rules
+
 
 addRule : Grammar -> String -> String -> Grammar
-addRule gram prod rule = 
+addRule gram prod rule =
     case gram of
         Grammar rules ->
             Grammar <| Dict.update prod (addProduction rule) rules
-            
-type ProdPart = Symbol String | Token String
-
-type Production = Terminal (List ProdPart) | NonTerminal (List ProdPart)
-
-type Rule = Rule String (List Production)
 
 
-parseRule : String -> String -> String
-parseRule sym prod = 
-    case run prodP prod of
-       Ok s ->
-        case s of 
-            is ->
-                "Symbol: " ++ (Debug.toString is)
-       Err _ ->
-        "Error"
-        
-
--- Parsers
-
-prodP : Parser (List ProdPart)
-prodP =
-    loop [] prodParser
-
-prodParser : List ProdPart -> Parser (Step (List ProdPart) (List ProdPart))
-prodParser revPps = oneOf 
-            [ succeed (\pp -> Loop (Token pp :: revPps)) |= (getChompedString (succeed () |. chompWhile (\c -> c /= '#')))
-            , succeed (\pp -> Loop (pp :: revPps)) |= prodSymbol]
+type ProdPart
+    = Symbol String
+    | Token String
 
 
-prodSymbol : Parser ProdPart
-prodSymbol = 
-        succeed Symbol
-        |=  (getChompedString <|
-            succeed ()
-            |. symbol "#"
-            |. chompUntil "#")
-    
+type Production
+    = Terminal (List ProdPart)
+    | NonTerminal (List ProdPart)
+
+
+type Rule
+    = Rule String (List Production)
+
+
+type RuleError
+    = Error
+
+
+parseRule : String -> String -> Rule
+parseRule sym prod =
+    let
+        newProd =
+            prodParser prod
+    in
+    case newProd of
+        Ok parsed ->
+            Rule sym [ parsed ]
+
+        Err _ ->
+            Rule sym []
+
+
+prodParser : String -> Result RuleError Production
+prodParser prod =
+    let
+        prodParts : Result RuleError (List ProdPart)
+        prodParts =
+            splitProdParts prod
+    in
+    case prodParts of
+        Ok parsed ->
+            if List.all symbol parsed then
+                Ok (NonTerminal (List.reverse parsed))
+
+            else
+                Ok (Terminal (List.reverse parsed))
+
+        Err x ->
+            Err x
+
+
+symbol : ProdPart -> Bool
+symbol prodPart =
+    case prodPart of
+        Symbol _ ->
+            True
+
+        Token _ ->
+            False
+
+
+splitProdParts : String -> Result RuleError (List ProdPart)
+splitProdParts prod =
+    -- List.foldl makeParts (Ok []) (Regex.split (Maybe.withDefault Regex.never (Regex.fromString "#(\\S+?)#")) prod)
+    List.foldl makeParts (Ok []) (String.split "#" prod)
+
+
+makeParts : String -> Result RuleError (List ProdPart) -> Result RuleError (List ProdPart)
+makeParts strPart done =
+    case done of
+        Ok parsed ->
+            case modBy 2 (List.length parsed) of
+                0 ->
+                    Result.map (\l -> Token strPart :: l) done
+
+                1 ->
+                    Result.map (\l -> Symbol strPart :: l) done
+
+                _ ->
+                    Err Error
+
+        Err x ->
+            Err x
+
+
+
+-- prodP : Parser (List ProdPart)
+-- prodP =
+--     loop [] prodParser
+-- prodParser : List ProdPart -> Parser (Step (List ProdPart) (List ProdPart))
+-- prodParser revPps =
+--     oneOf
+--         [ succeed (\pp -> Loop (Token pp :: revPps)) |= getChompedString (succeed () |. chompWhile (\c -> c /= '#'))
+--         , succeed (\pp -> Loop (pp :: revPps)) |= prodSymbol
+--         , succeed (Done (List.reverse revPps)) |. end
+--         ]
+-- prodSymbol : Parser ProdPart
+-- prodSymbol =
+--     succeed Symbol
+--         |= (getChompedString <|
+--                 succeed ()
+--                     |. symbol "#"
+--                     |. chompUntil "#"
+--            )
--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,32 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), blue, grammrows, header, init, inputRows, lblue, main, renderProduction, renderProductions, rowStyle, update, view)
 
 import Browser
-import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding, fillPortion, centerX)
+import Debug
+import Dict exposing (Dict)
+import Element exposing (Element, alignRight, centerX, centerY, el, fill, fillPortion, padding, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
 import Generator exposing (..)
-import Dict exposing (Dict)
-import Debug
 
-type alias Model = 
+
+type alias Model =
     { ntValue : String
     , prodValue : String
     , grammar : Grammar
     , ph : String
     }
 
+
 init : Model
-init = 
-    { ntValue = "" 
+init =
+    { ntValue = ""
     , prodValue = ""
     , grammar = Generator.empty
-    , ph = ""}
+    , ph = ""
+    }
+
 
 main =
     Browser.sandbox { init = init, update = update, view = view }
@@ -38,60 +42,94 @@ update msg model =
     case msg of
         NTermChange v ->
             { model | ntValue = v }
+
         ProdChange v ->
             { model | prodValue = v }
+
         Save ->
-            { model | grammar = addRule model.grammar model.ntValue model.prodValue, ph = Debug.log (parseRule "" model.prodValue) ""  }
+            { model
+                | grammar = addRule model.grammar model.ntValue model.prodValue
+                , prodValue = ""
+
+                -- , ph = Debug.log (Debug.toString (parseRule model.ntValue model.prodValue)) ""
+            }
+
+
 
 -- VIEW
 
-blue = Element.rgb 0.1 0.51 1
 
-lblue = Element.rgba 0.1 0.51 1 0.7
+blue =
+    Element.rgb 0.1 0.51 1
+
+
+lblue =
+    Element.rgba 0.1 0.51 1 0.7
+
 
 rowStyle : List (Element.Attribute msg)
-rowStyle = [width fill, spacing 10, padding 10]
+rowStyle =
+    [ width fill, spacing 10, padding 10 ]
 
 
 header : Element msg
-header = Element.row rowStyle [el [width <| fillPortion 1, Background.color blue] (Element.text "Symbol")
-                                                          , el [width <| fillPortion 5] (Element.text "Rule")]
+header =
+    Element.row rowStyle
+        [ el [ width <| fillPortion 1, Background.color blue ] (Element.text "Symbol")
+        , el [ width <| fillPortion 5 ] (Element.text "Rule")
+        ]
+
 
 renderProduction : String -> String -> Element msg
 renderProduction nt prod =
-    Element.row rowStyle [el [width <| fillPortion 1, Background.color lblue] (Element.text nt)
-                   , el [width <| fillPortion 5] (Element.text prod) ]
+    Element.row rowStyle
+        [ el [ width <| fillPortion 1, Background.color lblue ] (Element.text nt)
+        , el [ width <| fillPortion 5 ] (Element.text prod)
+        ]
+
 
 renderProductions : String -> List String -> List (Element msg) -> List (Element msg)
 renderProductions nt prods acc =
-    let 
+    let
         first =
             case List.head prods of
-            Just prod ->
-                renderProduction nt prod
-            Nothing ->
-                Element.none
-        rest = Maybe.withDefault [] (List.tail prods)
+                Just prod ->
+                    renderProduction nt prod
+
+                Nothing ->
+                    Element.none
+
+        rest =
+            Maybe.withDefault [] (List.tail prods)
     in
-    first :: List.map (renderProduction "")  rest ++ acc
-    
+    first :: List.map (renderProduction "") rest ++ acc
+
 
 grammrows : Grammar -> List (Element msg)
-grammrows gram = 
+grammrows gram =
     case gram of
         Grammar rules ->
             Dict.foldl renderProductions [] rules
 
-inputRows : Element Msg            
-inputRows = Element.row rowStyle [ Input.text [width <| fillPortion 1] {onChange = NTermChange
-                                            , label = Input.labelHidden "Symbol"
-                                            , placeholder = Just <| Input.placeholder [] (Element.text "Symbol")
-                                            , text = "" } 
-                            , Input.text [width <| fillPortion 4] {onChange = ProdChange
-                                            , label = Input.labelHidden "Production"
-                                            , placeholder = Just <| Input.placeholder []  (Element.text "Production")
-                                            , text = "" } 
-                            , Input.button [width <| fillPortion 1] {onPress= Just Save, label = Element.text "Add Rule"}]
+
+inputRows : Model -> Element Msg
+inputRows model =
+    Element.row rowStyle
+        [ Input.text [ width <| fillPortion 1 ]
+            { onChange = NTermChange
+            , label = Input.labelHidden "Symbol"
+            , placeholder = Just <| Input.placeholder [] (Element.text "Symbol")
+            , text = model.ntValue
+            }
+        , Input.text [ width <| fillPortion 4 ]
+            { onChange = ProdChange
+            , label = Input.labelHidden "Production"
+            , placeholder = Just <| Input.placeholder [] (Element.text "Production")
+            , text = model.prodValue
+            }
+        , Input.button [ width <| fillPortion 1 ] { onPress = Just Save, label = Element.text "Add Rule" }
+        ]
+
 
 view model =
-    Element.layout [] <| Element.column [width fill] <| header :: (List.append (grammrows model.grammar) [inputRows])
+    Element.layout [] <| Element.column [ width fill ] <| header :: List.append (grammrows model.grammar) [ inputRows model ]
