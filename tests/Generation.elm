772181db93fac4baf200391e368d368ef619f183
Swen Mulderij <swenmulderij@gmail.com>
2020-03-18 21:05:44 +0100

    before Grammar refactor

--- tests/Generation.elm
+++ tests/Generation.elm
@@ -1,57 +1,100 @@
 module Generation exposing (suite)
 
-import Expect exposing (Expectation, fail, pass, true, ok)
+import Debug
+import Dict exposing (Dict)
+import Expect exposing (Expectation, fail, ok, pass, true)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Grammar exposing (..)
 import Parser
-import Test exposing (..)
-import Dict
-import Dict exposing (Dict)
+import Random exposing (initialSeed, step)
 import Random.List exposing (shuffle)
-import Random exposing(step, initialSeed)
-import Debug
+import Test exposing (..)
 
-unpackedTestGrammar = Dict.fromList [("START", [Grammar.NonTerminal [Token "He saw a ", Symbol "animal", Token "in a ", Symbol "location"]])
-                                    ,("animal", [Grammar.Terminal [Token "dog"], Grammar.Terminal [Token "sheep"]])
-                                    ,("location", [Grammar.Terminal [Token "forest"], Grammar.Terminal [Token "city"]])]
+
+unpackedTestGrammar : Dict String (List Production)
+unpackedTestGrammar =
+    Dict.fromList
+        [ ( "START", [ Grammar.NonTerminal [ Token "He saw a ", Symbol "animal", Token "in a ", Symbol "location" ] ] )
+        , ( "animal", [ Grammar.Terminal [ Token "dog" ], Grammar.Terminal [ Token "sheep" ] ] )
+        , ( "location", [ Grammar.Terminal [ Token "forest" ], Grammar.Terminal [ Token "city" ] ] )
+        ]
+
+
 testGrammar : Grammar
-testGrammar = Grammar <| unpackedTestGrammar
+testGrammar =
+    Grammar <| unpackedTestGrammar
+
 
 testGrammarSentences : List (Result GeneratorError String)
-testGrammarSentences = [Ok "He saw a dog in a forest", Ok "He saw a sheep in a forest", Ok "He saw a dog in a city", Ok "He saw a sheep in a forest"]
+testGrammarSentences =
+    [ Ok "He saw a dog in a forest", Ok "He saw a sheep in a forest", Ok "He saw a dog in a city", Ok "He saw a sheep in a forest" ]
 
-type GeneratorError =
-    NoStartRule | SymbolMissing String
 
-pickRule : Dict String (List Production) -> String -> Result GeneratorError (List Production)
+possibleAnimals : List Production
+possibleAnimals =
+    [ Grammar.Terminal [ Token "dog" ], Grammar.Terminal [ Token "sheep" ] ]
+
+
+type GeneratorError
+    = NoStartRule
+    | SymbolMissing String
+    | EmptyProduction
+
+
+pickRule : Dict String (List Production) -> String -> Result GeneratorError Production
 pickRule gram symbol =
     case Dict.get symbol gram of
         Just rules ->
-            Ok (Tuple.first <| step (shuffle rules) (initialSeed 42))
+            let
+                choice =
+                    List.head <| Tuple.first <| step (shuffle rules) (initialSeed 42)
+            in
+            case choice of
+                Just prod ->
+                    Ok prod
+
+                Nothing ->
+                    Err EmptyProduction
+
         Nothing ->
             Err (SymbolMissing symbol)
+
+
+
+-- prodToString : Production -> Result GeneratorError String
+-- prodToString prod =
+--     case prod of
+--         Terminal pps ->
+--             Ok
+--         NonTerminal pps ->
+
 
 generateSentence : Grammar -> Result GeneratorError String
 generateSentence g =
     case g of
         Grammar gram ->
-            case Dict.get "START" gram of
-                Just rules ->
-                    Ok  "He saw a dog in a forest"
-                Nothing ->
-                    Err NoStartRule
+            if Dict.member "START" gram then
+                -- Ok <| pickRule gram "START"
+                Ok "He saw a dog in a forest"
+
+            else
+                Err NoStartRule
+
 
 suite : Test
 suite =
     describe "Check the generation of sentences"
         [ describe "A grammar produces valid sentences"
             [ test "a random generated sentence is in the list of valid sentences" <|
-                (\_ ->
-                    Expect.true "expect sentence to be in valid list" <| List.member (generateSentence testGrammar) testGrammarSentences)
+                \_ ->
+                    Expect.true "expect sentence to be in valid list" <| List.member (generateSentence testGrammar) testGrammarSentences
             ]
         , describe "Rule picking works"
-                [test "Picking a rule works" <|
-                (\_ ->
-                    Expect.ok <| Debug.log (Debug.toString (pickRule unpackedTestGrammar "START")) (pickRule unpackedTestGrammar "START"))]
-                 
+            [ test "Picking a rule works" <|
+                \_ ->
+                    Expect.ok <| pickRule unpackedTestGrammar "START"
+            , test "Picking a rule results in a valid rule" <|
+                \_ ->
+                    Expect.true "picking from animal results in an animal choice" <| List.member (Result.withDefault (Grammar.Terminal []) <| pickRule unpackedTestGrammar "animal") possibleAnimals
+            ]
         ]
